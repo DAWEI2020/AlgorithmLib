83e8475f52af9ce787599a2294feb67f3665bea7
wangdawei <849937249@qq.com>
2021-12-06 20:48:45 +0800

    updata

--- fitLine.py
+++ fitLine.py
@@ -2,59 +2,55 @@
 # https://blog.csdn.net/mago2015/article/details/84295425
 
 import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import RANSACRegressor
-
-
-def lin_regplot(X, y, model):
-    plt.scatter(X, y, c='blue')
-    plt.plot(X, model.predict(X), color='red')
-    plt.savefig('result/Linear.png')
-    plt.show()
-    return None
+from sklearn.datasets import make_regression
 
 
 # 线性回归模型
-# 数据读取
-df = pd.read_csv('dataset/boston.csv', sep=',')
-df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS',
-              'NOX', 'RM', 'AGE', 'DIS', 'RAD',
-              'TAX', 'PTRATIO', 'LSTAT', 'MEDV']
+df = pd.read_csv('dataset/line.csv', sep=',')
+df.columns = ['X', 'y']
 # print(df.head())
-
-X = df[['RM']].values
-y = df[['MEDV']].values
-
+X = df[['X']].values
+y = df[['y']].values
 slr = LinearRegression()
 slr.fit(X, y)
 print("Slope: %.3f" % slr.coef_[0])
 print("intercept: %.3f" % slr.intercept_)
-
-lin_regplot(X, y, slr)
+plt.scatter(X, y, c='blue')
+plt.plot(X, slr.predict(X), color='red')
+plt.savefig('result/line.png')
+plt.show()
 
 # 使用RANSAC清除异常值高鲁棒对的线性回归模型
-ransac = RANSACRegressor(LinearRegression(),
-                         max_trials=100,
-                         min_samples=50,
-                         # residual_metric=lambda x: np.sum(np.abs(x), axis=1),
-                         residual_threshold=5.0,
-                         random_state=0)
+ransac = RANSACRegressor(LinearRegression(), max_trials=100, min_samples=50, residual_threshold=0.2, random_state=0)
 ransac.fit(X, y)
-# 可视化
-inlier_mask = ransac.inlier_mask_
-outlier_mask = np.logical_not(inlier_mask)
-line_X = np.arange(3, 10, 1)
+in_mask = ransac.inlier_mask_
+out_mask = np.logical_not(in_mask)
+line_X = np.arange(1, 251, 1)
 line_y_ransac = ransac.predict(line_X[:, np.newaxis])
-plt.scatter(X[inlier_mask], y[inlier_mask],
-            c='blue', marker='o', label='Inliers')
-plt.scatter(X[outlier_mask], y[outlier_mask],
-            c='lightgreen', marker='s', label='Outliers')
-
+plt.scatter(X[in_mask], y[in_mask], c='blue', marker='o', label='Inliers')
+plt.scatter(X[out_mask], y[out_mask], c='green', marker='s', label='Outliers')
 plt.plot(line_X, line_y_ransac, color='red')
-plt.xlabel('Average number of rooms [RM]')
-plt.ylabel('Price in $ 1000 [MEDV]')
+plt.xlabel('Index')
+plt.ylabel('Height')
 plt.savefig('result/ransac.png')
+plt.show()
+
+print(np.arange(1, 10, 1))  # [1 2 3 4 5 6 7 8 9]
+
+X, y = make_regression(n_samples=200, n_features=1, noise=4.0, random_state=0)
+reg = RANSACRegressor(random_state=0).fit(X, y)
+reg.score(X, y)
+
+print(X.shape)
+print(y.shape)
+print(X)
+print(y)
+
+plt.scatter(X, y, c='blue')
+plt.plot(X, reg.predict(X), color='red')
+plt.savefig('result/ran.png')
 plt.show()
